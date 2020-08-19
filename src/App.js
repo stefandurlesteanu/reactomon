fc124c8cddce6d59738889f2a0afd31590e46da5
stefandurlesteanu <stefan.durlesteanu@gmail.com>
2020-08-19 22:09:08 +0300

    NavBar template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from './components/layout/Header'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 	return (
 		<div className="App">
+			<Header />
 		</div>
 	);
 }
