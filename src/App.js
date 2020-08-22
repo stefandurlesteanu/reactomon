8b968c5ed65496398c3d7bbc867bbd4649261555
stefandurlesteanu <stefan.durlesteanu@gmail.com>
2020-08-22 23:22:44 +0300

    fetched data with axios

--- src/App.js
+++ src/App.js
@@ -2,40 +2,47 @@ import React from 'react';
 import Header from './components/layout/Header';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Pokemons from './components/Pokemons';
 import Types from './components/pages/Types';
 import PokemonInfo from './components/PokemonInfo';
 import { useState, useEffect } from 'react';
-import { getAllPokemon } from './services/pokemon'
+import { getAllPokemon, getPokemon } from './services/pokemon'
 
 
 function App() {
 
 	const [pokemonData, setPokemonData] = useState([]);
-	const [nextUrl, setNextUrl] = useState('');
-	const [prevUrl, setPrevUrl] = useState('');
+	// const [nextUrl, setNextUrl] = useState('');
+	// const [prevUrl, setPrevUrl] = useState('');
 	const [loading, setLoading] = useState(true);
 	const initialUrl = 'https://pokeapi.co/api/v2/pokemon';
 
 	useEffect(() => {
 		async function fetchData() {
 			let response = await getAllPokemon(initialUrl);
-			setNextUrl(response.next);
-			setPrevUrl(response.previous);
+			// setNextUrl(response.next);
+			// setPrevUrl(response.previous);
+			await loadingPokemon(response.results);
 			setLoading(false);
-
 		}
 
 		fetchData();
 
 	}, [])
 
-	axios.get('https://pokeapi.co/api/v2/pokemon?limit=20')
-		.then(res => this.setState({ pokemons: res.data.results }))
+	const loadingPokemon = async (data) => {
+		let _pokemon = await Promise.all(data.map(async pokemon => {
+			let pokemonRecord = await getPokemon(pokemon.url);
+			return pokemonRecord
+		}))
+		setPokemonData(_pokemon)
+	}
 
-
+	console.log(pokemonData)
+	// console.log(nextUrl)
+	// console.log(prevUrl)
+	console.log(loading)
 	return (
 		<Router>
 			<div className="App" >
--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -3,8 +3,37 @@ import axios from 'axios';
 export async function getAllPokemon(url) {
 	return new Promise((resolve, reject) => {
 		axios.get(url)
-			.then(response => {
-				resolve(response.data);
+			.then(function ({ data }) {
+				resolve(data);
 			})
 	})
 }
+
+export async function getPokemon(url) {
+	return new Promise((resolve, reject) => {
+		axios.get(url)
+			.then(function ({ data }) {
+				resolve(data);
+			})
+	})
+}
+
+// export async function getAllPokemon(url) {
+// 	return new Promise((resolve, reject) => {
+// 		fetch(url)
+// 			.then(res => res.json())
+// 			.then(data => {
+// 				resolve(data);
+// 			});
+// 	});
+// }
+
+// export async function getPokemon(url) {
+// 	return new Promise((resolve, reject) => {
+// 		fetch(url)
+// 			.then(res => res.json())
+// 			.then(data => {
+// 				resolve(data);
+// 			});
+// 	});
+// }
